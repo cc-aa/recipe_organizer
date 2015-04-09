319ebd384ec3e316f6e9e85db863d207845f792e
Red A <hey.ccaa@gmail.com>
2015-04-09 16:26:17 -0600

    syntax error in urls.py fixed

--- backend/apps/recipes/views.py
+++ backend/apps/recipes/views.py
@@ -1,10 +1,9 @@
-from django.shortcuts import render
 from rest_framework import generics
 from serializers import RecipeSerializer
 from models import Recipe
 
 
-class RecipeList(generics.ListAPIVIew):
+class RecipeList(generics.ListAPIView):
     serializer_class = RecipeSerializer
     queryset = Recipe.objects.all()
 
